b336aea4729a8fd6266ff97a720265f67986153d
Dennis Kemboi <denkogei11@gmail.com>
2024-09-24 21:07:46 +0300

    arrow function

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (num1, num2) => {
+    return (num1 / num2);
+}
+console.log(divide(2000, 200))
+
+const square = (num1)  =>  {
+    return (num1 * num1);
+}
+const add = (num1,num2) =>(num1+num2)
+    
